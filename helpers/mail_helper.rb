607606b24d6b5d6daee48e539602c5a05c26fb8d
Dave Wu <eyaswoo@gmail.com>
2016-01-23 11:40:40 +0800

    fix: use timestamp to serialize in json

--- helpers/mail_helper.rb
+++ helpers/mail_helper.rb
@@ -4,7 +4,7 @@ helpers do
     subject = "韩非子说用户验证"
     content = "亲爱滴#{user.info.nickname}你好，请点击或复制链接 #{settings.site_host}/user/validation?id=#{user.id}&code=#{user.vcode} 以激活你的账户。成功激活后就可以提问和回答问题了。"
 
-    data = { to: to, subject: subject, content: content, time: Time.now }
+    data = { to: to, subject: subject, content: content, time: Time.now.to_i }
     send_mail_job(data)
   end
 

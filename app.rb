58053ee88d43fb8055d908463de55ef423f305a0
Dave Wu <eyaswoo@gmail.com>
2016-02-03 23:21:22 +0800

    add log;
    complete different config for dev and prod env.

--- app.rb
+++ app.rb
@@ -12,12 +12,51 @@ require_relative 'models/init'
 require_relative 'controllers/init'
 require_relative 'helpers/init'
 
+configure do
+  set :db_conf, {
+    :host     => "localhost",
+    :username => "dave",
+    :password => "123123",
+    :database => "xxx"
+  }
+
+  set :redis_conf, {
+    :host       => '127.0.0.1',
+    :port       => 6379,
+    :db         => 8,
+    :mail_queue => 'validation'
+  }
+
+  enable :mail_validation
+  disable :status_limit
+  set :site_host, 'http://localhost:4567'
+  set :public_folder, File.dirname(__FILE__) + '/public'
+  use Rack::Session::Pool, expire_after: 60 * 60 * 2, http_only: true
+
+  log_file = File.new("#{settings.root}/log/#{settings.environment}.log", 'a+')
+  log_file.sync = true
+  use Rack::CommonLogger, log_file
+end
+
+configure :production do
+  set :db_conf, {
+    :host     => "localhost",
+    :username => "root",
+    :password => "123123",
+    :database => "xxx"
+  }
+
+  enable :mail_validation
+  disable :status_limit
+  set :site_host, 'http://115.28.62.31:4567'
+end
+
 ActiveRecord::Base.establish_connection(
   :adapter  => "mysql2",
-  :host     => "localhost",
-  :username => "dave",
-  :password => "123123",
-  :database => "xxx"
+  :host     => settings.db_conf[:host],
+  :username => settings.db_conf[:username],
+  :password => settings.db_conf[:password],
+  :database => settings.db_conf[:database]
 )
 
 # let sinatra use correct timezone to save data
@@ -30,35 +69,7 @@ end
 
 # avoid db connection deadlock issue
 after do
+  logger.info "=== shutdown the db connection!"
   ActiveRecord::Base.connection.close
   headers @headers
-end
-
-# common config
-configure do
-  # redis conf for delayed mailing jobs
-  set :redis_conf, {
-    host: '127.0.0.1',
-    port: 6379,
-    mail_queue: 'validation',
-    db: 8
-  }
-
-  # used in mail (since site host may change)
-  set :site_host, 'http://localhost:4567'
-
-  # no need to send mail in develop env
-  disable :mail_validation
-  disable :status_limit
-
-  set :public_folder, File.dirname(__FILE__) + '/public'
-  use Rack::Session::Pool, expire_after: 60 * 60 * 2, http_only: true
-end
-
-configure :production do
-  # enable mailing on user register
-  enable :mail_validation
-  disable :status_limit   # for now, just disable this
-
-  set :site_host, 'http://115.28.62.31:4567'
 end

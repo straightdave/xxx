1c8e39b01ae4f1f73386cb8fcbbc5aced998dfe9
Dave Wu <eyaswoo@gmail.com>
2016-03-14 23:29:25 +0800

    account managing page
    to be continued

--- controllers/admin/account_manage.rb
+++ controllers/admin/account_manage.rb
@@ -1,29 +1,14 @@
 get '/admin/accounts' do
-  @user ||= User.find_by(login_name: session[:login_name])
-  if @user && @user.admin?
-    @title = "账户管理"
-    @navbar_active = "account_manage"
+  @title = "账户管理"
+  @navbar_active = "account_manage"
+  erb 'admin/account_manage'.to_sym, layout: 'admin/layout'.to_sym
+end
 
-    @accounts = case @user.role
-    when User::Role::SUPERADMIN
-      User.where(role: User::Role::ADMIN)
-    when User::Role::ADMIN
-      User.where("users.role <> #{ User::Role::ADMIN } AND users.role <> #{ User::Role::SUPERADMIN }")
-    else []
-    end
-
-    erb 'admin/account_manage'.to_sym, layout: 'admin/layout'.to_sym
+# for ajax invoke
+get '/admin/user/:login_name' do |login_name|
+  if user = User.find_by(login_name: login_name)
+    json user
   else
-    redirect to('/login?r=' + CGI.escape('/admin') + '&w=2')
+    json ret: "not_found"
   end
-end
-
-# for admin ajax invoke
-get '/admin/account/:uid' do |uid|
-  user = User.find_by(id: uid)
-  json user
-end
-
-post '/admin/account/:uid' do |uid|
-
 end
--- public/js/my-admin.js
+++ public/js/my-admin.js
@@ -1,21 +1,11 @@
 $().ready(function () {
-  $("a.show-detail").click(function () {
-    var id = $(this).attr("name");
 
-    var pane = $("div.account-detail-pane");
-    pane.empty();
+  $("button#btn-search-user").click(function () {
+    var name = $("input#search-username").val();
+    $.get('/admin/user/' + name, function (data, status) {
+      console.log(data);
 
-    $.get('/admin/account/' + id, function (data, status) {
-      pane.append(
-        "<p> ID: " + id + "</p> \
-        <p> name: " + data.login_name + "</p> \
-        <div class='btn-group' role='group'> \
-        <button class='btn btn-default btn-sm'>挂起</button> \
-        <button class='btn btn-default btn-sm'>禁用</button> \
-        <button class='btn btn-default btn-sm'>还原</button> \
-        </div> \
-        ");
+      
     });
-
   });
 });
